fc51d56ce2546bae87c03efd6a3c1f11317eab9b
Abrar33 <barumalik333@gmail.com>
2026-02-02 15:17:59 +0500

    test

--- app/views/it/it.tsx
+++ app/views/it/it.tsx
@@ -1,24 +1,32 @@
-import React from "react";
-import ServiceHeader from "../components/services_header";
-import ItServices from "./itServices";
-import SuccessStories from "@/app/views/Home/Stories";
-import DynamicServiceSection from "./DynamicService";
-import WhyChooses from "../components/dynamic_choose_us";
-import { ItChooseUs } from "@/app/data/choose_us";
+import React from 'react';
+import ServiceHeader from '../components/services_header';
+import ItServices from './itServices';
+import SuccessStories from '@/app/views/Home/Stories'
+import DynamicServiceSection from './DynamicService';
+import WhyChooses from '../components/dynamic_choose_us';
+import { ItChooseUs } from '@/app/data/choose_us';
 
+/**
+ * SoftwareServices Page Component
+ * This component assembles the Software Services view using various sections.
+ */
 const SoftwareServices: React.FC = () => {
   return (
     <>
-      <ServiceHeader
-        title="Software Services"
+      {/* Hero / Header Section */}
+      <ServiceHeader 
+        title="Software Services" 
         description="Our software services encompass a wide range of solutions designed to meet your business needs, from custom software development to system integration and maintenance."
-        image="https://ncrisolutions.com/wp-content/uploads/2025/08/IT-Services-Banner-2048x569.webp"
-      />
-
+        image="https://ncrisolutions.com/wp-content/uploads/2025/08/IT-Services-Banner-2048x569.webp"  
+      />      
+      
+      {/* IT Specific Services Grid */}
       <ItServices />
       <WhyChooses title="Why Choose Our IT Services?" features={ItChooseUs} />
+      {/* Customer Success Case Studies */}
       <SuccessStories />
-
+      
+      {/* Call to Action Banner */}
       <DynamicServiceSection
         layout="banner"
         image="https://images.pexels.com/photos/4974922/pexels-photo-4974922.jpeg"
